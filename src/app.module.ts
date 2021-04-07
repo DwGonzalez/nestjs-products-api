47518d1c7cb2372eb87bfe18ea598573cb6c80a1
dw <darwinmanuel1@hotmail.com>
2021-04-07 13:02:12 -0400

    - Create products module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from 'nestjs-dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule,
+    ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
