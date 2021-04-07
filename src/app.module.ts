20e0841e6835e6caf87eb928a51dd899d885f233
dw <darwinmanuel1@hotmail.com>
2021-04-07 14:42:03 -0400

    - Added MongoDB Connection - Create product Service functions - Create product interface - Create product schema - Added end-point to create product

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from 'nestjs-dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ProductModule,
-    ConfigModule.forRoot()],
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_CONN_STRING, { useNewUrlParser: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {
+}
